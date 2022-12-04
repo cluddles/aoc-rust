b415dcca9f08ef35fe3f83ee0ed987183afd77b9
Dan Fielding <dgfielding@gmail.com>
2022-12-04 00:27:19 +0000

    2022/03 solution

---
+++ src/bin/solution_2022_03.rs
@@ -0,0 +1,69 @@
+extern crate aoc;
+
+use aoc::shared;
+
+const DAY: &str = "2022/03";
+
+/// Split each line into 2 compartments
+fn parse_compartments(content: &str) -> Vec<Vec<&str>> {
+    shared::split_lines(content, false)
+        .iter()
+        .map(|x| x.split_at(x.len() / 2))
+        .map(|x| vec![x.0, x.1])
+        .collect()
+}
+
+/// Split by groups of 3 lines
+fn parse_elf_groups(content: &str) -> Vec<Vec<&str>> {
+    let lines = shared::split_lines(content, false);
+    let mut result = Vec::new();
+    for i in (0..lines.len()).step_by(3) {
+        result.push(vec![lines[i], lines[i + 1], lines[i + 2]]);
+    }
+    result
+}
+
+/// Find duplicates in the given strings
+fn dup(val: &[&str]) -> u8 {
+    val[0]
+        .chars()
+        .find(|&x| (1..val.len()).all(|y| val[y].contains(x)))
+        .unwrap() as u8
+}
+
+/// Convert char to priority value
+fn priority(val: u8) -> u8 {
+    match val {
+        65u8..=90u8 => val - 64u8 + 26u8,
+        97u8..=122u8 => val - 96u8,
+        _ => panic!(),
+    }
+}
+
+/// Sum of all duplicate char priorities
+fn sum_priority(input: &[Vec<&str>]) -> u32 {
+    input.iter().map(|x| priority(dup(x)) as u32).sum()
+}
+
+fn main() {
+    let content = shared::read_res_day(DAY, "input");
+    println!("Part 1: {}", sum_priority(&parse_compartments(&content)));
+    println!("Part 2: {}", sum_priority(&parse_elf_groups(&content)));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let content = shared::read_res_day(DAY, "input.test");
+        assert_eq!(sum_priority(&parse_compartments(&content)), 157);
+    }
+
+    #[test]
+    fn test_part2() {
+        let content = shared::read_res_day(DAY, "input.test");
+        assert_eq!(sum_priority(&parse_elf_groups(&content)), 70);
+    }
+}
