b4e371e32f7d7f3e2b588b32aab70e5c9659fe98
Dan Fielding <dgfielding@gmail.com>
2022-12-14 20:35:57 +0000

    Move 2022/04, 05 to libs

--- aoc-2022/src/main.rs
+++ aoc-2022/src/main.rs
@@ -1,6 +1,9 @@
+use std::time::SystemTime;
 use aoc_2022::day01::Day01;
 use aoc_2022::day02::Day02;
 use aoc_2022::day03::Day03;
+use aoc_2022::day04::Day04;
+use aoc_2022::day05::Day05;
 use aoc_2022::day06::Day06;
 use aoc_2022::day10::Day10;
 use aoc_2022::day11::Day11;
@@ -10,14 +13,20 @@ use aoc_2022::day14::Day14;
 use aoc_lib::harness::*;
 
 fn main() -> DynResult<()> {
+    let time = SystemTime::now();
+
     run_solution(&Day01)?;
     run_solution(&Day02)?;
     run_solution(&Day03)?;
+    run_solution(&Day04)?;
+    run_solution(&Day05)?;
     run_solution(&Day06)?;
     run_solution(&Day10)?;
     run_solution(&Day11)?;
     run_solution(&Day12)?;
     run_solution(&Day13)?;
     run_solution(&Day14)?;
+
+    println!("\nTotal time: {:?}", time.elapsed()?);
     Ok(())
 }
